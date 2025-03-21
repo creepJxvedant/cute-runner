29f6ac552d67510c16fef941a7436dc4e480b121
creepJxvedant <vedantverma303@gmail.com>
2025-03-21 10:12:14 +0530

    ok

--- javascripts/script.js
+++ javascripts/script.js
@@ -44,6 +44,90 @@ music.volume = 1;
 let assetsToLoad = 0;
 let assetsLoaded = 0;
 
+function startGame() {
+  let canvas = document.querySelector(".canvas"),
+    canvasWidth = (canvas.width = 0.98 * window.innerWidth),
+    canvasHeight = (canvas.height = window.innerHeight),
+    ctx = canvas.getContext("2d");
+
+  let collisionChecker = new u();
+  let playerInstance = new $(ctx, canvasHeight, PlayerSpeed);
+  player = playerInstance;
+
+  function spawnEntities() {
+    if (a.length < MAX_ENEMIES) {
+      let enemy = new f(ctx, i, 80);
+      a.push(enemy);
+    }
+    if (totalPlant < 3) {
+      let plant = new d(ctx);
+      a.push(plant);
+      totalPlant++;
+    }
+  }
+
+  let enemySpawner = setInterval(spawnEntities, 1000);
+
+  function gameLoop() {
+    if (player.isAlive) {
+      music.play();
+      ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+
+      // Update backgrounds
+      r.forEach((layer) => {
+        player.isAlive && layer.update();
+        layer.draw();
+      });
+
+      // Update and draw hearts
+      Hearts.forEach((heart) => {
+        heart.update();
+        heart.draw();
+      });
+
+      // Ensure at least one enemy is present
+      if (a.length === 0) spawnEntities();
+
+      // Update and draw enemies and plants
+      a.forEach((entity) => {
+        entity.update();
+        entity.draw();
+      });
+
+      // Update collision checker
+      if (player.isAlive) {
+        drawEnemyCount();
+        drawScore();
+        collisionChecker.update(ctx);
+      } else {
+        clearInterval(enemySpawner);
+      }
+
+      // Update and draw player
+      player.update();
+      player.draw();
+
+      requestAnimationFrame(gameLoop);
+    }
+  }
+
+  function drawEnemyCount() {
+    ctx.fillStyle = "#f04";
+    ctx.fillText(`EnemyCount : ${a.length}`, window.innerWidth - 198, 102, 100);
+    ctx.fillStyle = "black";
+    ctx.fillText(`EnemyCount : ${a.length}`, window.innerWidth - 200, 100, 100);
+  }
+
+  function drawScore() {
+    ctx.fillStyle = "#f04";
+    ctx.fillText(`score : ${player.score}`, 60, 100, 100);
+    ctx.fillStyle = "#000";
+    ctx.fillText(`score : ${player.score}`, 58, 98, 100);
+  }
+
+  gameLoop();
+}
+
 function trackAssetLoad() {
   assetsLoaded++;
   if (assetsLoaded === assetsToLoad) {
@@ -51,149 +135,53 @@ function trackAssetLoad() {
   }
 }
 
-window.addEventListener("DOMContentLoaded", () => {
-  let canvas = document.querySelector(".canvas"),
-    canvasWidth = (canvas.width = 0.98 * window.innerWidth),
-    canvasHeight = (canvas.height = window.innerHeight),
-    ctx = canvas.getContext("2d");
+function preloadAssets() {
+  assetsToLoad = 1; // Music counts as one asset
 
-  function preloadAssets() {
-    assetsToLoad = 1; // Music counts as one asset
-
-    // Preload player sprites
-    n.forEach((action) => {
-      l[action] = [];
-      for (let t = 0; t < 10; t++) {
-        let img = new Image();
-        img.src = `./player_sprites/${action}/${t}.png`;
-        img.onload = trackAssetLoad;
-        assetsToLoad++;
-        l[action].push(img);
-      }
-    });
-
-    // Preload enemy explosion sprites
-    s.forEach((enemyList) => {
-      enemyList.forEach((enemy) => {
-        i[enemy].src = [];
-        for (let t = 0; t < i[enemy].frames; t++) {
-          let img = new Image();
-          img.src = `./Enemies/skelton_explode/${enemy}/${t}.png`;
-          img.onload = trackAssetLoad;
-          assetsToLoad++;
-          i[enemy].src.push(img);
-        }
-      });
-    });
-
-    // Preload background layers
-    for (let u = 0; u < e.length; u++) {
+  // Preload player sprites
+  n.forEach((action) => {
+    l[action] = [];
+    for (let t = 0; t < 10; t++) {
       let img = new Image();
-      img.src = `./Background/Layer_${u}.png`;
+      img.src = `./player_sprites/${action}/${t}.png`;
       img.onload = trackAssetLoad;
       assetsToLoad++;
-      t.push(img);
+      l[action].push(img);
     }
-
-    t.reverse();
-    t.forEach((bg) => {
-      r.push(new c(bg, ctx));
-    });
-
-    // Preload music
-    music.oncanplaythrough = trackAssetLoad;
-  }
-
-  function startGame() {
-    let collisionChecker = new u();
-    let playerInstance = new $(ctx, canvasHeight, PlayerSpeed);
-    player = playerInstance;
-
-    function spawnEntities() {
-      if (a.length < MAX_ENEMIES) {
-        let enemy = new f(ctx, i, 80);
-        a.push(enemy);
-      }
-      if (totalPlant < 3) {
-        let plant = new d(ctx);
-        a.push(plant);
-        totalPlant++;
-      }
-    }
-
-    let enemySpawner = setInterval(spawnEntities, 1000);
-
-    function gameLoop() {
-      if (player.isAlive) {
-        music.play();
-        ctx.clearRect(0, 0, canvasWidth, canvasHeight);
-
-        // Update backgrounds
-        r.forEach((layer) => {
-          player.isAlive && layer.update();
-          layer.draw();
-        });
-
-        // Update and draw hearts
-        Hearts.forEach((heart) => {
-          heart.update();
-          heart.draw();
-        });
-
-        // Ensure at least one enemy is present
-        if (a.length === 0) spawnEntities();
-
-        // Update and draw enemies and plants
-        a.forEach((entity) => {
-          entity.update();
-          entity.draw();
-        });
-
-        // Update collision checker
-        if (player.isAlive) {
-          drawEnemyCount();
-          drawScore();
-          collisionChecker.update(ctx);
-        } else {
-          clearInterval(enemySpawner);
-        }
-
-        // Update and draw player
-        player.update();
-        player.draw();
-
-        requestAnimationFrame(gameLoop);
-      }
-    }
-
-    function drawEnemyCount() {
-      ctx.fillStyle = "#f04";
-      ctx.fillText(
-        `EnemyCount : ${a.length}`,
-        window.innerWidth - 198,
-        102,
-        100
-      );
-      ctx.fillStyle = "black";
-      ctx.fillText(
-        `EnemyCount : ${a.length}`,
-        window.innerWidth - 200,
-        100,
-        100
-      );
-    }
-
-    function drawScore() {
-      ctx.fillStyle = "#f04";
-      ctx.fillText(`score : ${player.score}`, 60, 100, 100);
-      ctx.fillStyle = "#000";
-      ctx.fillText(`score : ${player.score}`, 58, 98, 100);
-    }
-
-    gameLoop();
-  }
-
-  window.addEventListener("load", () => {
-    preloadAssets();
   });
+
+  // Preload enemy explosion sprites
+  s.forEach((enemyList) => {
+    enemyList.forEach((enemy) => {
+      i[enemy].src = [];
+      for (let t = 0; t < i[enemy].frames; t++) {
+        let img = new Image();
+        img.src = `./Enemies/skelton_explode/${enemy}/${t}.png`;
+        img.onload = trackAssetLoad;
+        assetsToLoad++;
+        i[enemy].src.push(img);
+      }
+    });
+  });
+
+  // Preload background layers
+  for (let u = 0; u < e.length; u++) {
+    let img = new Image();
+    img.src = `./Background/Layer_${u}.png`;
+    img.onload = trackAssetLoad;
+    assetsToLoad++;
+    t.push(img);
+  }
+
+  t.reverse();
+  t.forEach((bg) => {
+    r.push(new c(bg, document.querySelector(".canvas").getContext("2d")));
+  });
+
+  // Preload music
+  music.oncanplaythrough = trackAssetLoad;
+}
+
+window.addEventListener("load", () => {
+  preloadAssets();
 });
